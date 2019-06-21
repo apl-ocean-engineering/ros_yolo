34763fe7be112a304bd8b90a2ec4d4e69d9370e1
miscott <miscott@uw.edu>
2019-06-20 17:43:30 -0700

    Slowly building the node...

--- src/yolo.py
+++ src/yolo.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import CameraInfo, Image
-from pytorchYolo import argLoader
+from pytorchYolo import argLoader, detector
 
 
 if __name__ == "__main__":
     name = "info_pub"
     rospy.init_node(name)
+    argloader = argLoader.ArgLoader()
+    print(argloader.args)
     
     
